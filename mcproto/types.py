06a914ce261cc9b34f93aeecc91f4a8a334765fc
Eric Chai <electromatter@gmail.com>
2016-12-16 03:54:39 -0500

    added docstring to mcproto/types.py

--- mcproto/types.py
+++ mcproto/types.py
@@ -1,1 +1,44 @@
+"""
+This file contains the compiler intrinsics for the builtin types.
+
+=== Simple Types ===
+
+Integral types:
+ - bool
+ - varint
+ - varlong
+ - ubyte, byte
+ - ushort, short
+ - uint, int
+ - ulong, long
+ - unsigned, signed
+
+Floating-point types:
+ - float, double
+
+Fixed-point types:
+ - position (3-vector)
+ - angle
+
+Structured types:
+ - metadata, nbt
+
+=== Parameterized Types ===
+
+String types:
+ - bytes, string, uuid
+
+Array types:
+ - array
+ - bool_optional
+
+=== Special Types ===
+
+Enum types:
+ - enum
+ - flag
+
+"""
+
+
 
